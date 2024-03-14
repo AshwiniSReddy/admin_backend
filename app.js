2e627da1624556e291c163c507363d9118b3e5be
digital@paraminnovation.org <digital@paraminnovation.org>
2024-03-14 16:57:35 +0530

    added https://admindashboard.paramscience.org

--- app.js
+++ app.js
@@ -50,9 +50,13 @@ app.use(
         maxAge: 24 * 60 * 60 * 1000 // Corrected to 24 hours in milliseconds
     })
 );
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 const corsOptionsDelegate = function (req, callback) {
     var corsOptions;
-    let allowedOrigins = ['http://localhost:3000','https://paramscience.org','https://admindashboard.paramscience.org','http://dashboardadmin.s3-website.ap-south-1.amazonaws.com']; // You can add more origins as needed
+    let allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:3000','https://paramscience.org','https://admindashboard.paramscience.org','http://dashboardadmin.s3-website.ap-south-1.amazonaws.com']; // You can add more origins as needed
     
     if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
       corsOptions = { origin: true, methods: "GET,POST,PUT,DELETE", credentials: true }; // Reflect (enable) the requested origin in the CORS response
